5341639f31ad45c427e1dbe962dab8c1943aa8da
mussab_tafal <mussab@procure.ps>
2024-03-02 18:39:58 +0400

    Finalizing Section 4Sedsasdafsd

---
+++ section 4/tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function Player({initialName, symbol, isActive}) {
+    const [isEditing, setIsEditing] = useState(false)
+    const [name, setName] = useState(initialName)
+
+    let editablePlayer = <span className="player-name">{name}</span>;
+    if(isEditing) {
+        editablePlayer = <span><input type="text" required value={name} onChange={handleChange}/></span>
+    };
+
+    function handleClick() {
+        setIsEditing(() => !isEditing)
+    };
+
+    function handleChange(event) {  
+        setName(event.target.value)
+    };
+
+    return (
+        <li className={isActive? 'active': undefined}>
+            <span className="player">
+              {editablePlayer}
+              <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleClick}>{isEditing? 'Save': 'Edit'}</button>
+          </li>
+    );
+}
